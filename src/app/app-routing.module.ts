0687b784bb6904fa6a2838f10e6e4b8cc3265f3d
micael <micaelsn>
2019-11-21 19:44:32 -0300

    lista de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  {
+    path: 'products',
+    children: [
+      {
+        path: '',
+        loadChildren: './products/products.module#ProductsPageModule',
+      },
+      {
+        path: ':productId',
+        loadChildren: './products/product-details/product-details.module#ProductDetailsPageModule'
+      },
+    ]
+  },
 ];
 
 @NgModule({
